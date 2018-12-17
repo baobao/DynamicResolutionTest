db8a787d7e3f6b89f536c71bd9c725a7abb7a6b5
baobao <shunsuke.ohba@gmail.com>
2018-12-17 19:02:47 +0900

    Add DynamicResolution Sample

---
+++ Assets/Scripts/DynamicResolutionTest.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DynamicResolutionTest : MonoBehaviour
+{
+    private RenderTexture _renderTexture;
+
+    [SerializeField]
+    private Texture2D _tex;
+
+    /// <summary>
+    /// RenderTexture確認用UI
+    /// </summary>
+    [SerializeField]
+    private RawImage _previewRenderTexture;
+
+    private Camera _camera;
+
+
+    /// <summary>
+    /// 毎フレームSclableBufferの値を書き換えてみるテストフラグ
+    /// </summary>
+    private bool _isUpdateScalableBuffer = false;
+
+    private float _updateScale = 0;
+
+    void Start()
+    {
+        _camera = Camera.main;
+
+        _renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
+
+        _previewRenderTexture.texture = _renderTexture;
+    }
+
+    void Blit()
+    {
+        // RenderTextureへ書き込み
+        Graphics.Blit(_tex, _renderTexture);
+    }
+
+    void BlitFrameBuffer()
+    {
+        // カメラのレンダーターゲットにRenderTextureをセットして描画
+        _camera.targetTexture = _renderTexture;
+        _camera.Render();
+
+        // カメラのレンダーターゲットを元のフレームバッファへ戻す
+        _camera.targetTexture = null;
+
+        Debug.Log("cam.allowDynamicResolution : " + _camera.allowDynamicResolution);
+    }
+
+    void Update()
+    {
+        if (_isUpdateScalableBuffer)
+        {
+            if (_updateScale > 1.0f)
+            {
+                _updateScale = 0.01f;
+            }
+
+            ScalableBufferManager.ResizeBuffers(_updateScale, _updateScale);
+            _updateScale += 0.01f;
+        }
+    }
+
+    void OnGUI()
+    {
+        float buttonW = 200f;
+        float buttonH = 60f;
+        if (GUILayout.Button("Blit", GUILayout.Width(buttonW), GUILayout.Height(buttonH)))
+        {
+            Blit();
+        }
+
+        if (GUILayout.Button("Screen.SetResolution", GUILayout.Width(buttonW), GUILayout.Height(buttonH)))
+        {
+            int w = Screen.width / 10;
+            int h = Screen.height / 10;
+            Screen.SetResolution(w, h, FullScreenMode.ExclusiveFullScreen);
+        }
+
+        if (GUILayout.Button("RT Toggle : " + _renderTexture.useDynamicScale, GUILayout.Width(buttonW),
+            GUILayout.Height(buttonH)))
+        {
+            // RenderTextureのDynamicResolution切り替え
+            _renderTexture.useDynamicScale = _renderTexture.useDynamicScale == false;
+        }
+
+        if (GUILayout.Button("Camera Toggle : " + _camera.allowDynamicResolution, GUILayout.Width(buttonW),
+            GUILayout.Height(buttonH)))
+        {
+            // CameraのDynamicResolution切り替え
+            _camera.allowDynamicResolution = _camera.allowDynamicResolution == false;
+        }
+
+        if (GUILayout.Button("W1.0 : H1.0", GUILayout.Width(buttonW), GUILayout.Height(buttonH)))
+        {
+            ScalableBufferManager.ResizeBuffers(1.0f, 1.0f);
+        }
+
+        if (GUILayout.Button("W0.5 : H0.5", GUILayout.Width(buttonW), GUILayout.Height(buttonH)))
+        {
+            ScalableBufferManager.ResizeBuffers(0.5f, 0.5f);
+        }
+
+        if (GUILayout.Button("W0.1 : H0.1", GUILayout.Width(buttonW), GUILayout.Height(buttonH)))
+        {
+            ScalableBufferManager.ResizeBuffers(0.1f, 0.1f);
+        }
+
+        if (GUILayout.Button(" Reset W1.0 : H0.3", GUILayout.Width(buttonW), GUILayout.Height(buttonH)))
+        {
+            // 縦横比を崩してみる
+            ScalableBufferManager.ResizeBuffers(1.0f, 0.3f);
+        }
+
+
+        if (GUILayout.Button("Blit FrameBuffer", GUILayout.Width(buttonW), GUILayout.Height(buttonH)))
+        {
+            BlitFrameBuffer();
+        }
+
+        if (GUILayout.Button("Toggle OnUpdate Scalable : " + _isUpdateScalableBuffer, GUILayout.Width(buttonW),
+            GUILayout.Height(buttonH)))
+        {
+            _isUpdateScalableBuffer = _isUpdateScalableBuffer == false;
+        }
+
+
+        if (GUILayout.Button("CameraTargetTexture is Null", GUILayout.Width(buttonW), GUILayout.Height(buttonH)))
+        {
+            // targetTextureにnull突っ込むとSclableBufferが使えなくなる
+            _camera.targetTexture = null;
+        }
+    }
+}
---
+++ Assets/Scripts/RotateCube.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    private float _angle;
+
+    void Update()
+    {
+        _angle = Time.deltaTime * 20f;
+        transform.Rotate(new Vector3(0, 1f, 0), _angle);
+    }
+}
